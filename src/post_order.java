15bad659316e3de67d0cb4f723d75c276f59bbbd
ADITYA GUPTA <78367281+adityagupta0123@users.noreply.github.com>
2022-08-18 09:20:47 +0530

    Create post_order.java

---
+++ src/post_order.java
@@ -0,0 +1,23 @@
+
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {        
+         List<Integer> list = new ArrayList<>();
+        if( root == null)
+            return list;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while( !stack.isEmpty() ){
+            TreeNode curr = stack.pop();
+            list.add(0,curr.val);
+            
+            if(curr.left != null)
+                stack.push(curr.left);
+            
+            if( curr.right != null ) 
+                stack.push(curr.right);
+            
+        }
+        return list;
+                    
+    }
+}
